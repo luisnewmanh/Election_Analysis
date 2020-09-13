e886f7b29a58142635d6ba9fa4d87670f6956d9e
luisnewmanh <luis.newmanh@gmail.com>
2020-09-12 22:37:34 -0400

    Adding the election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -18,5 +18,31 @@ with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     # Print the header row.
     headers = next(file_reader)
-    print(headers)
-
+    #total number of votes
+    total_votes=0
+    #name of candidates
+    candidates_options=[]
+    #create dictonary
+    candidate_votes={}
+    #winner candidate tracker
+    winning_candidate = ""
+    winning_count = 0
+    winning_percentage = 0
+    for row in file_reader:
+        total_votes=total_votes+1
+        if row[2] not in candidates_options:
+            candidates_options.append(row[2])
+            candidate_votes[row[2]]=0
+        candidate_votes[row[2]]=candidate_votes[row[2]]+1
+    for candidate in candidates_options:
+        vote_percentage=(candidate_votes.get(candidate)/total_votes)*100
+        if candidate_votes.get(candidate)>winning_count and vote_percentage>winning_percentage:
+            winning_count=candidate_votes.get(candidate)
+            winning_percentage=vote_percentage
+            winning_candidate=candidate
+        print(f"{candidate}: {round(vote_percentage,1)}% ({candidate_votes.get(candidate)})\n")
+    print(f"---------------------------\n"
+        f"winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count}\n"
+        f"Winning Percentage: {round(winning_percentage,1)}%\n"
+        f"---------------------------")
