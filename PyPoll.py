3ec2c192523e9bd018e3b7944bf8815283dc85ed
luisnewmanh <luis.newmanh@gmail.com>
2020-09-13 11:27:49 -0400

    upload final versions

--- PyPoll.py
+++ PyPoll.py
@@ -22,17 +22,12 @@ with open(file_to_load) as election_data:
     total_votes=0
     #name of candidates
     candidates_options=[]
-    #name of counties
-    counties_names=[]
-    #create dictonaries candidates and votes
+    #create dictonary
     candidate_votes={}
-    county_votes={}
-    #winner candidate and largest county tracker
+    #winner candidate tracker
     winning_candidate = ""
     winning_count = 0
     winning_percentage = 0
-    largest_turnout=""
-    winning_county=0
     #vote count
     for row in file_reader:
         total_votes=total_votes+1
@@ -40,39 +35,18 @@ with open(file_to_load) as election_data:
             candidates_options.append(row[2])
             candidate_votes[row[2]]=0
         candidate_votes[row[2]]=candidate_votes[row[2]]+1
-        if row[1] not in counties_names:
-            counties_names.append(row[1])
-            county_votes[row[1]]=0
-        county_votes[row[1]]=county_votes[row[1]]+1
     #print results and save them into analysis.txt
     with open(file_to_save,"w") as txt_file:
         election_results = (
             f"\nElection Results\n"
             f"---------------------------\n"
             f"Total Votes: {total_votes:,}\n"
-            f"---------------------------\n\n")
+            f"---------------------------\n")
         print(election_results, end="")
         txt_file.write(election_results)
-        print("County Votes:")
-        txt_file.write("County Votes:\n")
-        for county in counties_names:
-            county_percentage=(county_votes.get(county)/total_votes)*100
-            county_results=(f"{county}: {round(county_percentage,1)}% ({county_votes.get(county)})\n")
-            print(county_results, end="")
-            txt_file.write(county_results)
-            if county_votes.get(county)>winning_county:
-                largest_turnout=county
-        greatest_turnout=(
-            f"\n"
-            f"""---------------------------
-Largest County Turnout: {largest_turnout}
----------------------------\n\n"""
-        )
-        print(greatest_turnout)
-        txt_file.write(greatest_turnout)
         for candidate in candidates_options:
             vote_percentage=(candidate_votes.get(candidate)/total_votes)*100
-            candidate_results=(f"{candidate}: {round(vote_percentage,1)}% ({candidate_votes.get(candidate)})\n\n")
+            candidate_results=(f"{candidate}: {round(vote_percentage,1)}% ({candidate_votes.get(candidate)})\n")
             print(candidate_results, end="")
             txt_file.write(candidate_results)
             if candidate_votes.get(candidate)>winning_count and vote_percentage>winning_percentage:
@@ -80,10 +54,9 @@ Largest County Turnout: {largest_turnout}
                 winning_percentage=vote_percentage
                 winning_candidate=candidate        
         winning_summary=(f"---------------------------\n"
-            f"Winner: {winning_candidate}\n"
+            f"winner: {winning_candidate}\n"
             f"Winning Vote Count: {winning_count}\n"
             f"Winning Percentage: {round(winning_percentage,1)}%\n"
             f"---------------------------")
         print(winning_summary, end="")
         txt_file.write(winning_summary)
-        #faltan comas en votos y error en turnout debe ser denver
--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -22,12 +22,17 @@ with open(file_to_load) as election_data:
     total_votes=0
     #name of candidates
     candidates_options=[]
-    #create dictonary
+    #name of counties
+    counties_names=[]
+    #create dictonaries candidates and votes
     candidate_votes={}
-    #winner candidate tracker
+    county_votes={}
+    #winner candidate and largest county tracker
     winning_candidate = ""
     winning_count = 0
     winning_percentage = 0
+    largest_turnout=""
+    winning_county=0
     #vote count
     for row in file_reader:
         total_votes=total_votes+1
@@ -35,28 +40,53 @@ with open(file_to_load) as election_data:
             candidates_options.append(row[2])
             candidate_votes[row[2]]=0
         candidate_votes[row[2]]=candidate_votes[row[2]]+1
+        if row[1] not in counties_names:
+            counties_names.append(row[1])
+            county_votes[row[1]]=0
+        county_votes[row[1]]=county_votes[row[1]]+1
     #print results and save them into analysis.txt
     with open(file_to_save,"w") as txt_file:
         election_results = (
             f"\nElection Results\n"
             f"---------------------------\n"
             f"Total Votes: {total_votes:,}\n"
-            f"---------------------------\n")
+            f"---------------------------\n\n")
         print(election_results, end="")
         txt_file.write(election_results)
+        print("County Votes:")
+        txt_file.write("County Votes:\n")
+        for county in counties_names:
+            county_percentage=(county_votes.get(county)/total_votes)*100
+            county_results=(f"{county}: {round(county_percentage,1)}% ({county_votes.get(county):,})\n")
+            print(county_results, end="")
+            txt_file.write(county_results)
+            if county_votes.get(county)>winning_county:
+                winning_county=county_votes.get(county)
+                largest_turnout=county
+        greatest_turnout=(
+            f"\n"
+            f"---------------------------\n"
+            f"Largest County Turnout: {largest_turnout}\n"
+            f"---------------------------\n\n"
+        )
+        print(greatest_turnout)
+        txt_file.write(greatest_turnout)
         for candidate in candidates_options:
             vote_percentage=(candidate_votes.get(candidate)/total_votes)*100
-            candidate_results=(f"{candidate}: {round(vote_percentage,1)}% ({candidate_votes.get(candidate)})\n")
+            candidate_results=(f"{candidate}: {round(vote_percentage,1)}% ({candidate_votes.get(candidate):,})\n")
             print(candidate_results, end="")
             txt_file.write(candidate_results)
             if candidate_votes.get(candidate)>winning_count and vote_percentage>winning_percentage:
                 winning_count=candidate_votes.get(candidate)
                 winning_percentage=vote_percentage
                 winning_candidate=candidate        
-        winning_summary=(f"---------------------------\n"
-            f"winner: {winning_candidate}\n"
-            f"Winning Vote Count: {winning_count}\n"
+        winning_summary=(
+            f"\n"
+            f"---------------------------\n"
+            f"Winner: {winning_candidate}\n"
+            f"Winning Vote Count: {winning_count:,}\n"
             f"Winning Percentage: {round(winning_percentage,1)}%\n"
-            f"---------------------------")
+            f"---------------------------"
+            )
         print(winning_summary, end="")
         txt_file.write(winning_summary)
